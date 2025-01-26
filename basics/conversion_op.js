c1c0cd468a722d92f8cc5d68752484018ba6cc52
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-01-26 13:27:38 +0000

    Conversion of datatypes

---
+++ basics/conversion_op.js
@@ -0,0 +1,23 @@
+let score = "35";
+
+console.log(typeof score)
+console.log(typeof (score));
+
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber );
+
+
+//"33"--->33
+//"33abc"--->NaN  Not a number
+//true-->1  false-->0
+
+let isLoggedIn = "rajeshwari"
+let BooleanisLoggedIn  = Boolean(isLoggedIn);
+console.log(typeof BooleanisLoggedIn );
+console.log(BooleanisLoggedIn);
+
+//1=> true  0=> false
+//null =>false
+//"rajeshwari" =>true
+
