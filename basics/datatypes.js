7d283f0eb2d52b77c82cf323a4cc14017ba141fe
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-01-26 13:10:28 +0000

    Datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use Strict";  // treate all js code as a newer version
+
+console.log (3 + 3); 
+console.log("Nalbalwar") // Code redability should be high
+
+//alert(1 +2) ----We are using node js not Browser 
+
+//Datatypes-->
+
+let name = "Rajeshwari"   //String
+let age = 21              //number
+let isLoggedIn = true;     //Boolean
+
+
+//Null   -- standalone value
+//undefined
+//symbol  --Unique
+
+console.log(typeof "Rajeshwari");
+console.log(typeof 21);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+//Object
