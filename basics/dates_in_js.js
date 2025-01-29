e8416653217327f8e8d486d8d822137e5e3fd6e2
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-01-29 08:59:53 +0000

    Date and time

---
+++ basics/dates_in_js.js
@@ -0,0 +1,15 @@
+let myDate = new Date();
+console.log(myDate);
+                                 //Output
+console.log(myDate.toString());  //Wed Jan 29 2025 08:47:20 GMT+0000 (Coordinated Universal Time)
+
+console.log(myDate.toDateString()); //Wed Jan 29 2025
+
+console.log(myDate.toLocaleDateString()); //1/29/2025
+
+console.log(myDate.toLocaleString()); //1/29/2025, 8:49:06 AM
+
+console.log(typeof myDate); //Object
+
+console.log(myDate.getMonth());
+console.log(myDate.getDate());
